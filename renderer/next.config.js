c6bbea0ac778b67a4ba78278902d28d6428cd243
oscelest <oscelest@gmail.com>
2019-06-16 00:30:03 +0200

    Next config should be added.

---
+++ renderer/next.config.js
@@ -0,0 +1,7 @@
+// const withTypescript = require("@zeit/next-typescript");
+// const withLess       = require("@zeit/next-less");
+
+module.exports = {
+  pageExtensions: ["js", "jsx"],
+  assetPrefix:    "./",
+};
