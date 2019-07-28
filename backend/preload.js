2fb3f39602d1ee0d81b24f38abded798311d3bb2
oscelest <oscelest@gmail.com>
2019-07-28 09:38:53 +0200

    Delete preload.js

--- backend/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron = require("electron");
-process.once("loaded", () => {
-    global.ipc = electron.ipcRenderer;
-});
